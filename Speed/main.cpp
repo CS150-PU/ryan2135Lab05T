9a4e8af076d76c0d8839c0803e457c6b74d77b1a
Doug Ryan <ryan2135@pacificu.edu>
2021-09-27 15:23:09 -0700

    Add project files.

---
+++ Speed/main.cpp
@@ -0,0 +1,19 @@
+//**********************************************************************
+// File:				main.cpp
+// Author:			
+// Date:				
+// Class:				
+// Assignment:	
+// Purpose:			
+// Hours:
+//**********************************************************************
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+	return EXIT_SUCCESS;
+}
